db8fa3525949631e3bfb0040e502d0a7ecdab14c
Anton Dobrynin <hashlib@yandex.ru>
2022-12-16 12:37:13 +0300

    Update welcome text

--- handlers/welcome.py
+++ handlers/welcome.py
@@ -2,5 +2,14 @@ from aiogram import types
 
 
 async def send_welcome(message: types.Message):
-    # TODO: fill help command reply
-    await message.reply("Hi!\nI'm To Do Bot!\n")
+    await message.reply('Hi!\n'
+                        "I'm To Do Bot!\n"
+                        'You can communicate with me through the commands:\n'
+                        '/add - Add new task'
+                        '/show - Show list of tasks\n'
+                        '/done - Mark task as done\n'
+                        '/undone - Mark task as undone\n'
+                        '/clear - Clear the whole task list\n'
+                        '/delete - Delete a particular task\n'
+                        '/help - Show this help\n'
+                        'You can also see this command in the menu')
